43407bee84eba03eb917f896f44a96bc30e347ad
valentinarizzo21 <rizzo.val3ntina@gmail.com>
2024-11-18 17:04:29 +0100

    welcome page, da sistemare parte finale

---
+++ assets/js/welcome.js
@@ -0,0 +1,18 @@
+const checkbox = document.getElementById("termsCheckbox");
+const errorMessage = document.getElementById("errorMessage");
+const bntWelcome = document.getElementById("bntWelcome");
+
+bntWelcome.addEventListener("click", function () {
+  if (!checkbox.checked) {
+    errorMessage.style.display = "block";
+  } else {
+    errorMessage.style.display = "none";
+    window.location.href = "../../benchmark.html";
+  }
+});
+
+checkbox.addEventListener("change", function () {
+  if (checkbox.checked) {
+    errorMessage.style.display = "none";
+  }
+});
