935a0eeaea11b6ec0c06ed7e00f7376324197544
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2024-11-19 23:00:05 +0100

    Commit chart

---
+++ assets/js/results.js
@@ -0,0 +1,13 @@
+const answerChart = document.getElementById("chart");
+new Chart(answerChart, {
+  type: 'doughnut',
+  data: {
+    labels: ['Red', 'Blue'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19],
+      borderWidth: 1
+    }]
+  },
+
+});
