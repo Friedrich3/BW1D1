a932ad30183791a4367473a304ad02ab1480194b
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2024-11-21 14:35:22 +0100

    commit pre-merge

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -4,21 +4,7 @@ const stars = document.querySelectorAll('.ratingStars .star');
 let isFocus = false;
 
 document.addEventListener('DOMContentLoaded', () => {
-
-    inputFeedback.addEventListener("mouseover", function(){
-        isFocus = true;
-    });
-    inputFeedback.addEventListener("mouseout", function(){
-        isFocus = false;
-    });
-    inputFeedback.addEventListener("blur", function(){
-        if(inputFeedback.value === ""){
-            updateStars(-1, 'selected');
-            updateStars(-1, 'hover');
-        }
-    });
-    
-    
+ 
     const updateStars = (index, className) => { 
         stars.forEach((s, i) => {
             s.classList.toggle(className, i <= index);
@@ -26,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     stars.forEach((star, index) => {
-        star.addEventListener('mouseover', () => updateStars(index, 'hover'));
+        star.addEventListener('mouseover', () => updateStars(index, 'hover'));              //EVENT LISTENER CHE GESTISCE IL FUNZIONAMENTO DELLE STELLE 
         star.addEventListener('mouseout', () => updateStars(-1, 'hover'));
         star.addEventListener('click', () => {
             updateStars(index, 'selected');
@@ -40,17 +26,35 @@ document.addEventListener('DOMContentLoaded', () => {
         });
        
     });
+    inputFeedback.addEventListener("mouseover", function(){                             //EVENT LISTENER CHE GESTISCE IL FOCUS delle stelle relativo alla imput box
+        isFocus = true; 
+    });
+    inputFeedback.addEventListener("mouseout", function(){
+        isFocus = false;
+    });
+    inputFeedback.addEventListener("blur", function(){
+        if(inputFeedback.value === ""){
+            updateStars(-1, 'selected');
+            updateStars(-1, 'hover');
+        }
+    });
+    btnFeedback.addEventListener("mouseover", function(){                                   //EVent listener che gestisce il focus sul bottone
+        isFocus = true;
+    });
+    
 });
 
 
-function submitForm(){
+function submitForm(){                                                                  //FUNZIONE Onclick sul bottone che colleziona dati se il feedback è stato inserito e collega ad un altra pagina
     if(stars[0].classList.contains("selected")){
     localStorage.setItem("votoForm" , countStar());
     localStorage.setItem("commentForm", inputFeedback.value);
-    window.location.href = "../../thanks.html"
+    window.location.href = "../../thanks.html";                         
+}else{
+    window.location.href = "../../welcome\\.html";
 }
 }
-function countStar(){
+function countStar(){                                                                   //funzione per ricavare il "voto ricevuto"
     let counterStar = 0;
     stars.forEach (element =>{
         if(element.classList.contains("selected")){
