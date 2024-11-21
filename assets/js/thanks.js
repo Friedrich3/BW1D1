514330ec13bddb172b35129d073aab4a7f221f97
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2024-11-21 17:13:34 +0100

    Aggiunti link page thanks

---
+++ assets/js/thanks.js
@@ -0,0 +1,11 @@
+const btnResult = document.getElementById("btnResult");
+const btnBack = document.getElementById("btnBack");
+
+
+function toResult(){
+    window.location.href="../../results.html";
+}
+
+function toMainPage(){
+    window.location.href="../../welcome.html";
+}
